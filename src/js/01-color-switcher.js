9a38c9500c0986b1b14952dae1395d4cd4b92886
Yana Babak <yana.i.babak@gmail.com>
2023-03-26 15:16:45 +0300

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+let timerId = null;
+
+btnStart.addEventListener('click', () => {
+  btnStart.parentNode.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    btnStart.parentNode.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.disabled = true;
+});
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
